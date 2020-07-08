4e55a72ed348d1abcc9d3be44c5b1f0a3d120f8f
himi <himanijain668@gmail.com>
2020-07-08 17:36:37 +0530

    Single Login Done

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Gate from './pages/Login/GatekeeperLogin';
 import MainPage from './pages/Mainpage/MainPage';
 import AdminDash from './pages/AdminDash/AdminDash';
-import Admin from './pages/Login/AdminLogin';
-import StudentLog from './pages/Login/StudentLogin';
+
 import StudentRegister from './pages/StudentRegister/StudentRegister';
 import Excel2 from './pages/Excel/Excel2';
 import Assessment from './pages/Assesment/Assessment';
@@ -17,9 +15,7 @@ export default () => (
         <Switch>
             <Route exact path="/Demo" component={AdminDash} />
             <Route exact path="/" component={MainPage} />
-            <Route exact path="/Login" component={Admin} />
-            <Route exact path="/Login3" component={StudentLog} />
-            <Route exact path="/Login4" component={Gate} />
+            
             <Route exact path='/Excel' component={Excel}></Route>
             <Route exact path='/App' component={StudentRegister}></Route>
             <Route exact path='/Excel2' component={Excel2}></Route>
